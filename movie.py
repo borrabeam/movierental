2d2bcb60162e234aa71113f3b42af49bbe321594
borrabeam <bannakorn.s@ku.th>
2021-11-07 16:23:18 +0700

    Add Attributes to movie.py

--- movie.py
+++ movie.py
@@ -6,17 +6,20 @@ class Movie:
 	A movie available for rent.
 	"""
 	
-	def __init__(self, title, price_code):
-		# Initialize a new movie. 
-		self.title = title
-		self.price_code = price_code
-
-	def get_price_code(self):
-		# get the price code
-		return self.price_code
+	def __init__(self, title, year, genre=list):
+		# Initialize a new movie.
+		self.__title = title
+		self.__year = year
+		self.__genre = genre
 	
 	def get_title(self):
-		return self.title
+		return self.__title
+	
+	def get_year(self):
+		return self.__year
+	
+	def get_genre(self):
+		return self.__genre
 	
 	def __str__(self):
-		return self.title
+		return self.__title
